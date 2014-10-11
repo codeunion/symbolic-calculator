353bf98f0876fa8a763e2376cff7ab38069c8b18
Albert Agram <albert.agram@gmail.com>
2014-10-11 09:09:32 +0000

    v1 solution with Stack

--- lib/expression.rb
+++ lib/expression.rb
@@ -1,4 +1,8 @@
+require "stack"
+
 class UndefinedVariableError < StandardError; end
+
+
 
 class Expression
 
@@ -12,6 +16,41 @@ class Expression
   # @param bindings [Hash] a map of variable names to concrete values
   # @return [Expression] Returns a possibly-simplified Expression
   def evaluate(bindings = {})
-    "Implement Expression#evaluate in #{__FILE__}"
+    stack = Stack.new
+
+    tokens.each do |token|
+      if numeric?(token)
+        stack.push(token.to_i)
+      elsif variable?(token)
+        raise UndefinedVariableError if bindings[token.to_sym].nil?
+        stack.push(bindings[token.to_sym])
+      elsif token == "*"
+        rhs = stack.pop
+        lhs = stack.pop
+        stack.push(lhs * rhs)
+      elsif token == "+"
+        rhs = stack.pop
+        lhs = stack.pop
+        stack.push(lhs + rhs)
+      else
+        raise "hmmm what token is this?"
+      end
+    end
+
+    stack.pop
+  end
+
+  private
+
+  def tokens
+    @expr.split(" ")
+  end
+
+  def numeric?(token)
+    token =~ /^-?\d+$/
+  end
+
+  def variable?(token)
+    token =~ /^[a-z]/
   end
 end
